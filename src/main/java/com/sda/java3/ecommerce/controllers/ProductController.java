d6d02d095caf8b455ad094967c46b938a0d5b9d8
Krist Apostol <kristapostol98@gmail.com>
2021-07-25 17:11:56 +0200

    product details

--- src/main/java/com/sda/java3/ecommerce/controllers/ProductController.java
+++ src/main/java/com/sda/java3/ecommerce/controllers/ProductController.java
@@ -2,18 +2,26 @@ package com.sda.java3.ecommerce.controllers;
 
 import com.sda.java3.ecommerce.services.product.ProductService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ProductController {
+    protected final ProductService productService;
+
+    public ProductController(final ProductService productRepository) {
+        this.productService = productRepository;
+    }
 
     @GetMapping("/products")
     public String home() {
         return "product-list";
     }
 
-    @GetMapping("/product-detail")
-    public String productDetail() {
+    @GetMapping("/product-detail/{id}")
+    public String productDetail(@PathVariable String id, ModelMap modelMap) {
+        modelMap.addAttribute("product", productService.getProductById(id));
         return "product-detail";
     }
 }
--- src/main/java/com/sda/java3/ecommerce/services/product/ProductService.java
+++ src/main/java/com/sda/java3/ecommerce/services/product/ProductService.java
@@ -7,4 +7,5 @@ import java.util.List;
 public interface ProductService {
     List<Product> getFeaturedProducts();
     void createDummyProducts();
+    Product getProductById(String id);
 }
